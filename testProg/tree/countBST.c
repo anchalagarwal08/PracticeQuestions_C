790360859ad726ea111ed19e3c7d51d7d5f92164
Your Name <you@example.com>
2015-07-12 07:52:28 -0700

    minor change

--- testProg/tree/countBST.c
+++ testProg/tree/countBST.c
@@ -2,25 +2,41 @@
 #include <string.h>
 #include <string.h>
 #include "Node.h"
-int countBST(node *root, int low, int high, int *count)
+#include <stdbool.h>
+bool isleaf(node *root)
+{
+	return (root->left ==NULL && root->right==NULL);
+}
+
+bool isValidNode(node *root, int low, int high)
+{
+	if(root->data >= low && root->data<=high)
+		return true;
+	else
+		return false;
+}
+bool countBST(node *root, int low, int high, int *count)
 {
 	if(root==NULL)
-	  return -1;
-	if(root->data >= low && root->data<=high)
+	 return true;
+
+	bool l = root->left ?countBST(root->left, low, high, count) : true;
+	bool r = root->right ?countBST(root->right, low, high, count): true;
+	if(l && r && isValidNode(root,low, high))
 	{
-	  printf("\n subtree rooted at: %d\n", root->data);
-	  (*count)++;
+		printf("\n data node=%d\n", root->data);
+		(*count)++;
+		return true;
+
 	}
-	countBST(root->left, low, high, count);
-	countBST(root->right, low, high, count);
-	return *count;
-} 
+	return false;
+}
 
 int main()
 {
 	node *root = createBST();
 	int count=0;	
-	int ret = countBST(root, 4, 29, &count);
-	printf("\n count=%d\n", ret);
+	int ret = countBST(root, 4, 30, &count);
+	printf("\n count=%d\n", count);
 	return 0;
 }
